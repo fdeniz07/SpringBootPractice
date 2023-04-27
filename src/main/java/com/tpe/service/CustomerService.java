67bc4d472033ce1aea707e35463234e104e2e8e4
Fatih Deniz <deniz.fatih@hotmail.de>
2023-04-27 18:51:18 +0200

    Initial commit

---
+++ src/main/java/com/tpe/service/CustomerService.java
@@ -0,0 +1,11 @@
+package com.tpe.service;
+
+import com.tpe.domain.Customer;
+import org.springframework.stereotype.Service;
+
+@Service
+public class CustomerService {
+
+    public void saveCustomer(Customer customer) {
+    }
+}
